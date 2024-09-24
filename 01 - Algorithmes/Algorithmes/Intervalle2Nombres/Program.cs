4e136c08fbb8a257bfaf24c48571c33558361eda
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-09-24 09:15:28 +0200

    Transfert de dossiers

---
+++ 01 - Algorithmes/Algorithmes/Intervalle2Nombres/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+spacename Intervalle2Nombres
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int A;
+            int B;
+
+            Console.Write("A : ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("B : ");
+            B = int.Parse(Console.ReadLine());
+
+            if(A < B)
+            {
+                for(A; A < B; A++)
+                {
+                    Console.Write()
+                }
+            }
+
+        }
+    }
+}
