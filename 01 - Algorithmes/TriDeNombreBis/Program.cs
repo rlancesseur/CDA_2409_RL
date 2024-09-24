a77502acb745a452ad328a161d17670a48170d02
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-09-24 08:24:08 +0200

    Fin TriDeNombreBis

---
+++ 01 - Algorithmes/TriDeNombreBis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TriDeNombreBis
+{
+    public class TriDeNombreBis
+    {
+        static public void Main(string[] args)
+        {
+            int[] tableau = new int[3];
+
+            for(int i = 0; i < tableau.Length; i++)
+            {
+                Console.Write("Saisissez un nombre : ");
+                tableau[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(tableau);
+            Array.Reverse(tableau);
+
+            for(int i = 0; i < tableau.Length; i++)
+            {
+                Console.Write(tableau[i] + " ");
+            }
+
+        }
+    }
+}
