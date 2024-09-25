4942f7f05ce283e74c8347832cadea625604db5b
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-09-25 12:37:33 +0200

    Fin CoursesBarnabe

---
+++ 01 - Algorithmes/Algorithmes/CourseBarnabe/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseBarnabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double S;
+            double moitieS;
+            int nombreMagasin = 0;
+
+            Console.WriteLine("Somme dont Barnabé dispose : ");
+            S = int.Parse(Console.ReadLine());
+
+            while (S != 0)
+            {
+                moitieS = S / 2;
+                S = moitieS - 1;
+                
+                if(S < 0)
+                {
+                    S = 0;
+                }
+
+                nombreMagasin += 1;
+            }
+
+            Console.WriteLine("Barnabé a fait ses courses dans " + nombreMagasin + " magasins.");
+
+        }
+    }
+}
