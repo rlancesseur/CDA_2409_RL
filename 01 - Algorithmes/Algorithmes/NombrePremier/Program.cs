974475a914371bdfd34c656d8918fce9ae771987
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-09-25 08:41:14 +0200

    Update nombrePremier

--- 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
+++ 01 - Algorithmes/Algorithmes/NombrePremier/Program.cs
@@ -9,28 +9,31 @@
 
             Console.Write("N : ");
             N = int.Parse(Console.ReadLine());
-            
-            for(int i = 2; i * i <= N; i++)
-            {
-                if (N % i == 0)
-                {
-                    nombrePremier = false;
-                }
 
-                else
-                {
-                    nombrePremier = true;
-                }
-            }
-
-            if (nombrePremier)
-            {
-                Console.WriteLine(N + " est un nombre premier");
-            }
-            else
+            if (N == 1)
             {
                 Console.WriteLine(N + " n'est pas un nombre premier");
             }
+
+            else if (N == 2)
+            {
+                Console.WriteLine(N + " est un nombre premier");
+            }
+
+
+                for (int i = 2; i < N; i++)
+                {
+                    if (N % i == 0)
+                    {
+                        Console.Write(N + " n'est pas un nombre premier");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(N + " est un nombre premier");
+                    }
+                }
         }
     }
 }
