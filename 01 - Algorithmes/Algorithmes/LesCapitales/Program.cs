40fe771cb1e7a566ad87590cea4bbba717645b75
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-02 12:17:25 +0200

    Fin LesCapitales

---
+++ 01 - Algorithmes/Algorithmes/LesCapitales/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LesCapitales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ?pays;
+            string capital;
+
+            Console.WriteLine("Saisissez un nom de pays");
+            pays = Console.ReadLine();
+
+            capital = CapitalCity(pays);
+
+            Console.WriteLine(capital);
+
+        }
+
+        static string CapitalCity(string _pays)
+        {
+            /*
+            if (_pays == "France")
+            {
+                return "Paris";
+            }
+            else if (_pays == "Allemagne")
+            {
+                return "Berlin";
+            }
+            else
+            {
+                return "Capital inconnue";
+            }
+            */
+
+            switch(_pays)
+            {
+                case "France":
+                    return "Paris";
+                    break;
+                case "Allemagne":
+                    return "Berlin";
+                    break;
+                default:
+                    return "Capitale inconnue";
+            }
+
+        }
+    }
+}
