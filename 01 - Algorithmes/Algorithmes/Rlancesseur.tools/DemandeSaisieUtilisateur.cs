9e4a8e280d6e061e8faa82e59b74d14fa09bc2f2
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-03 15:55:56 +0200

    Update Exercice

---
+++ 01 - Algorithmes/Algorithmes/Rlancesseur.tools/DemandeSaisieUtilisateur.cs
@@ -0,0 +1,51 @@
+﻿namespace Rlancesseur.tools
+{
+    public class DemandeSaisieUtilisateur
+    {
+        public static int DemanderNombreEntier(string _question)
+        {
+            string saisieUtilisateur;
+            int valeurRetour;
+            bool saisieOk;
+
+            do
+            {
+                Console.WriteLine(_question);
+                saisieUtilisateur = Console.ReadLine() ?? "";
+
+                saisieOk = int.TryParse(saisieUtilisateur, out valeurRetour);
+
+                if(!saisieOk)
+                {
+                    Console.WriteLine("Saisie invalide, recommencez !");
+                }
+
+            } while (!saisieOk);
+
+            return valeurRetour;
+        }
+
+        public static float DemanderNombreFloatPositif(string _question)
+        {
+            string saisieUtilisateur;
+            float valeurRetour;
+            bool saisieOk;
+
+            do
+            {
+                Console.WriteLine(_question);
+                saisieUtilisateur = Console.ReadLine() ?? "";
+
+                saisieOk = float.TryParse(saisieUtilisateur, out valeurRetour) && valeurRetour >= 0;
+
+                if(!saisieOk)
+                {
+                    Console.WriteLine("Saisie invalide, recommencez");
+                }
+
+            } while (!saisieOk);
+
+            return valeurRetour;
+        }
+    }
+}
