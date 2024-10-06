a870b9411f598088e28bebc65b41ac1bb7935eb5
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-10-06 09:48:40 +0200

    Update ControleRegex

---
+++ 01 - Algorithmes/Algorithmes/Rlancesseur.tools/ControleRegex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Rlancesseur.tools
+{
+    public class ControleRegex
+    {
+        public static bool ControleNom(string _nom)
+        {
+            string formatNom = "^[a-zàâéèëêïîôöùüûç]{2,32}$";
+
+            if(Regex.IsMatch(_nom, formatNom, RegexOptions.IgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
