61789358d73b64f34a65a62b7b4d6bc6650208cb
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-07 10:57:53 +0200

    Update MotDePasse

--- 01 - Algorithmes/Algorithmes/MotDePasseSecure/Program.cs
+++ 01 - Algorithmes/Algorithmes/MotDePasseSecure/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using Rlancesseur.tools;
+using System.Text.RegularExpressions;
 
 namespace MotDePasseSecure
 {
@@ -21,27 +22,40 @@ namespace MotDePasseSecure
         static void Main(string[] args)
         {
             string motDePasse;
+            bool motDePasseOk;
+            /*
             string regexMinuscules;
             string regexMajuscules;
             string regexChiffres;
             string regexCaracteresSpeciaux;
 
 
-            regexMinuscules = @"[a-z]{1,}"; // On ne met pas de ^ et $ car on veut juste que le mdp contienne des lettres (pas QUE des lettres)
+            regexMinuscules = @"[a-z]{1,}";  On ne met pas de ^ et $ car on veut juste que le mdp contienne des lettres (pas QUE des lettres)
             regexMajuscules = @"[A-Z]{1,}";
             regexChiffres = @"[0-9]+";
-            regexCaracteresSpeciaux = @"[^a-zA-Z-0-9]+"; // le ^ dans les crochets veut dire l'inverse
-
-            // motDePasse = motDePasse.Length < 5 ? "ok" : "pas ok";
+            regexCaracteresSpeciaux = @"[^a-zA-Z0-9]+";  le ^ dans les crochets veut dire l'inverse
+            */
 
             Console.WriteLine("Saisir un mot de passe");
             motDePasse = Console.ReadLine() ?? ""; // null coalescing operator
 
-
+            /*
             if (Regex.IsMatch(motDePasse, regexMinuscules) &&
                 Regex.IsMatch(motDePasse, regexMajuscules) &&
                 Regex.IsMatch(motDePasse, regexChiffres) &&
                 (Regex.IsMatch(motDePasse, regexCaracteresSpeciaux) || motDePasse.Length > 20) && motDePasse.Length >= 12)
+            {
+                Console.WriteLine("Mot de passe OK");
+            }
+            else
+            {
+                Console.WriteLine("Mot de passe trop faible");
+            }
+            */
+
+            motDePasseOk = ControleRegex.ControleMotDePasse(motDePasse);
+
+            if (motDePasseOk)
             {
                 Console.WriteLine("Mot de passe OK");
             }
