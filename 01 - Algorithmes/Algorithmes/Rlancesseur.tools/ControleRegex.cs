6a461f4df1d7dd59e76a65ae39f5eab66330bbb3
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-07 10:58:01 +0200

    Update ControleRegex.cs

--- 01 - Algorithmes/Algorithmes/Rlancesseur.tools/ControleRegex.cs
+++ 01 - Algorithmes/Algorithmes/Rlancesseur.tools/ControleRegex.cs
@@ -13,14 +13,35 @@ namespace Rlancesseur.tools
         {
             string formatNom = "^[a-zàâéèëêïîôöùüûç]{2,32}$";
 
-            if(Regex.IsMatch(_nom, formatNom, RegexOptions.IgnoreCase))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return Regex.IsMatch(_nom, formatNom, RegexOptions.IgnoreCase);
+        }
+
+        public static bool ControleMotDePasse(string _motDePasse)
+        {
+            string formatMotDepasse = "^([a-z]+)([A-Z]+)([0-9]+)([^a-zA-Z0-9]+){12, }$";
+
+            return (Regex.IsMatch(_motDePasse, formatMotDepasse));
+        }
+
+        public static bool ControleFormatCarteBleue(string _carteBleue)
+        {
+           string formatCarteBleue = @"^([0-9]{4}[ -]?){4}$";
+
+            return (Regex.IsMatch(_carteBleue, formatCarteBleue));
+        }
+
+        public static bool ControleNumeroTelephone(string _numeroTelephone)
+        {
+            string formatNumeroTelephone = @"^([0-9]{1}[ -]?){10}$";
+
+            return Regex.IsMatch(_numeroTelephone, formatNumeroTelephone);
+        }
+
+        public static bool ControleMail(string _mail)
+        {
+            string formatMail = @"^([a-z0-9.?-?]{1, })(@{1}){5, 64}([a-z]{1, })(.{1})([a-z]{2-10}){5, 320}$";
+
+            return Regex.IsMatch(_mail, formatMail);
         }
     }
 }
