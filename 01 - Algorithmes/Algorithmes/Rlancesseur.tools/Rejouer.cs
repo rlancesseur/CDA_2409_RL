84dd1395926093be1be29a8112444129cf00147f
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-09 14:06:06 +0200

    Create Rejouer.cs

---
+++ 01 - Algorithmes/Algorithmes/Rlancesseur.tools/Rejouer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rlancesseur.tools
+{
+    public class Rejouer
+    {
+        public static bool RejouerProgramme()
+        {
+            bool result = true;
+
+            Console.WriteLine("Recommencer une partie ? (o/n)");
+            ConsoleKeyInfo loop = Console.ReadKey(true);
+
+            if (loop.KeyChar == 'o')
+            {
+                result = true;
+            }
+            else if (loop.KeyChar == 'n')
+            {
+                result = false;
+            }
+
+            return result;
+
+        }
+    }
+}
