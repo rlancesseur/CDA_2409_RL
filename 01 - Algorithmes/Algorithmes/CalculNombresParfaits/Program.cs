a73e4a5e6cf45afba51a8a4ccb52eae8b9080261
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2024-10-10 15:54:36 +0200

    Update Exercice

---
+++ 01 - Algorithmes/Algorithmes/CalculNombresParfaits/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculNombresParfaits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limiteNombresParfait = 0;
+            int somme = 0;
+
+            while ( limiteNombresParfait < 4)
+            {
+                for (int i = 0; i < i / 2; i++)
+                {
+                    somme += i;
+
+                    if()
+                }
+            }
+        }
+    }
+}
---
+++ 01 - Algorithmes/Algorithmes/EnregistrementUtilisateurs/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace EnregistrementUtilisateurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+                         Au démarrage, il n'y a aucun utilisateur enregistré.
+
+            ## Déroulement du programme
+
+            1. Le programme demande à l'utilisateur de saisir un nom et un prénom.
+                  - L’utilisateur saisit un nom et un prénom.
+
+            2. Le programme demande à l'utilisateur de saisir la date de naissance.
+                  - L'utilisateur saisit la date de naissance.
+
+            3. Le programme calcule l'âge de la personne en cours d'ajout.
+                  - Si l’âge est supérieur ou égal à 18 (majeur)
+                        - Le programme demande à l'utilisateur de saisir son métier.
+                  - Si l’âge est inférieur à 18 (mineur)
+                        - Le programme demande à l'utilisateur de saisir sa couleur préférée.
+
+            4. Lorsque toutes les informations sont saisies
+                  - Le programme enregistre l'utilisateur
+
+            5. Le programme demande à l'utilisateur s'il souhaite ajouter une autre personne.
+                  - Si oui
+                        - Retour à l'étape 1 (saisir nom et prénom)
+                  - Si non
+                        - Afficher tous les utilisateurs enregistrés en respectant ce format :
+                        - Nom Prénom - Date de naissance (âge) - Métier/Couleur préférée
+
+            6. Le programme remercie l'utilisateur et se termine
+
+            */
+
+            string saisieNomPrenom;
+            int dateNaissance;
+            int age;
+
+
+        }
+    }
+}
+
+
