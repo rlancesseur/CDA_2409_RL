13ad23c0cab06f33194b6384a91b0aeeca023ba5
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-10-26 10:41:17 +0200

    Ajout saisie manuelle

--- 02 - Webfront/Exercices-JS/intro_2/assets/Js/script.js
+++ 02 - Webfront/Exercices-JS/intro_2/assets/Js/script.js
@@ -1,8 +1,11 @@
 let boutonPlus = document.querySelector("#augmente")
 let boutonMoins = document.querySelector("#diminue")
-let zoneTaille = document.querySelector("span")
+let zoneTaille = document.querySelector("#zoneTaille")
 let taille = parseInt(zoneTaille.textContent)
 let paragraphe = document.querySelector("#text")
+let saisie = document.querySelector("#saisie")
+let boutonValider = document.querySelector("#validerSaisie")
+
 
 function augmenterTaille() {
     if(taille < 48) {
@@ -30,6 +33,17 @@ function diminuerTaille() {
     }
 }
 
-boutonPlus.addEventListener("click", augmenterTaille);
-boutonMoins.addEventListener("click", diminuerTaille)
+function saisirTaille() {
+    if(saisie.value > 7 && saisie.value < 49) {
+        taille = saisie.value
+        zoneTaille.innerText = taille
+        paragraphe.style.fontSize = taille + "px"
+    }
+    else {
+        alert("Veuillez entrer un nombre entre 8 et 48.");
+      }
+}
 
+boutonPlus.addEventListener("click", augmenterTaille)
+boutonMoins.addEventListener("click", diminuerTaille)
+boutonValider.addEventListener("click", saisirTaille)
