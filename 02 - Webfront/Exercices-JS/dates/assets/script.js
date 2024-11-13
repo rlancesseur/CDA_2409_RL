df8283a57f8137571794fbcc0e7072f39606f421
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-11-13 20:11:24 +0100

    omg les signes astro

--- 02 - Webfront/Exercices-JS/dates/assets/script.js
+++ 02 - Webfront/Exercices-JS/dates/assets/script.js
@@ -24,46 +24,44 @@ function affichage() {
         zoneDateNaissance.innerText = "Vous êtes né le " + dateFr + " à " + date[1]
         zoneNbAnnee.innerText = "Il s'est écoulé " + intervalle + " années depuis votre naissance."
         
-        switch (dateNaissance) {
-            case jour >= 20 && mois === 1 || jour <= 18 && mois === 2:
-                signeAstro = "Verseau"
-                break
-            case jour >= 19 && mois === 2 || jour <= 20 && mois === 3:
-                signeAstro = "Poissons"
-                break
-            case jour >= 21 && mois === 3 || jour <= 19 && mois === 4:
-                signeAstro = "Bélier"
-                break
-            case jour >= 20 && mois === 4 || jour <= 20 && mois === 5:
-                signeAstro = "Taureau"
-                break
-            case jour >= 21 && mois === 5 || jour <= 20 && mois === 6:
-                signeAstro = "Gémeaux"
-                break
-            case jour >= 21 && mois === 6 || jour <= 22 && mois === 7:
-                signeAstro = "Cancer"
-                break
-            case jour >= 23 && mois === 7 || jour <= 22 && mois === 8:
-                signeAstro = "Lion"
-                break
-            case jour >= 23 && mois === 8 || jour <= 22 && mois === 9:
-                signeAstro = "Vierge"
-                break
-            case jour >= 23 && mois === 9 || jour <= 22 && mois === 10:
-                signeAstro = "Balance"
-                break
-            case jour >= 23 && mois === 10 || jour <= 21 && mois === 11:
-                signeAstro = "Scorpion"
-                break
-            case jour >= 22 && mois === 11 || jour <= 21 && mois === 12:
-                signeAstro = "Sagittaire"
-                break
-            case jour >= 22 && mois === 12 || jour <= 20 && mois === 1:
-                signeAstro = "Capricorne"
-                break
-
-            default:
-                signeAstro = "Erreur"
+        if ((jour >= 20 && mois === 1) || (jour <= 18 && mois === 2)) {
+            signeAstro = "Verseau";
+          } 
+          else if ((jour >= 19 && mois === 2) || (jour <= 20 && mois === 3)) {
+            signeAstro = "Poissons";
+          }
+          else if ((jour >= 21 && mois === 3) || (jour <= 19 && mois === 4)) {
+            signeAstro = "Bélier"
+          }
+          else if ((jour >= 20 && mois === 4) || (jour <= 20 && mois === 5)) {
+            signeAstro = "Taureau"
+          }
+          else if ((jour >= 21 && mois === 5) || (jour <= 20 && mois === 6)) {
+            signeAstro = "Gémeaux"
+          }
+          else if ((jour >= 21 && mois === 6) || (jour <= 22 && mois === 7)) {
+            signeAstro = "Cancer"
+          }
+          else if ((jour >= 23 && mois === 7 ) || (jour <= 22 && mois === 8)) {
+            signeAstro = "Lion"
+          }
+          else if ((jour >= 23 && mois === 8) || (jour <= 22 && mois === 9)) {
+            signeAstro = "Vierge"
+          }
+          else if ((jour >= 23 && mois === 9) || (jour <= 22 && mois === 10)) {
+            signeAstro = "Balance"
+          }
+          else if ((jour >= 23 && mois === 10) || (jour <= 21 && mois === 11)) {
+            signeAstro = "Scorpion"
+          }
+          else if ((jour >= 22 && mois === 11 ) || (jour <= 21 && mois === 12)) {
+            signeAstro = "Sagittaire"
+          }
+          else if ((jour >= 22 && mois === 12) || (jour <= 20 && mois === 1)) {
+            signeAstro = "Capricorne"
+          }
+          else {
+            signeAstro = "Erreur"
         }
 
         zoneAstro.innerText = "Votre signe astrologique : " + signeAstro
