779d21625e8f3146bbce1020a66a0341121568dd
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-11-20 18:36:54 +0100

    Update algo & event

--- 02 - Webfront/Sites/Shifumi/assets/script.js
+++ 02 - Webfront/Sites/Shifumi/assets/script.js
@@ -5,73 +5,73 @@ const zoneResultat = document.querySelector("#zoneResultat")
 const imgPierre = document.querySelector("#imgPierre")
 const imgPapier = document.querySelector("#imgPapier")
 const imgCiseaux = document.querySelector("#imgCiseaux")
+let affichageGagnant = document.createElement("p")
+
+let saisieUtilisateur = 0
+
+imgPierre.addEventListener("click", () => {
+    saisieUtilisateur = 1
+    nouvellePartie()
+})
+imgPapier.addEventListener("click", () => {
+    saisieUtilisateur = 2
+    nouvellePartie()
+})
+imgCiseaux.addEventListener("click", () => {
+    saisieUtilisateur = 3
+    nouvellePartie()
+})
 
 btnCommencer.addEventListener("click", () => {
     btnCommencer.style.display = "none"
     wrapper.style.display = "flex"
     zoneScore.style.display = "flex"
+    zoneResultat.style.display = "none"
 })
+
 
 const nouvellePartie = () => {
     let zoneScoreUtilisateur = document.querySelector("#zoneScoreUtilisateur")
     let zoneScoreOrdinateur = document.querySelector("#zoneScoreOrdinateur")
     let scoreUtilisateur = 0
     let scoreOrdinateur = 0
+    zoneScoreUtilisateur.innerText = scoreUtilisateur
+    zoneScoreOrdinateur.innerText = scoreOrdinateur
 
     while(scoreUtilisateur < 3 && scoreOrdinateur < 3) {
 
-        let saisieUtilisateur = 0
-        let saisieOrdinateur = 0
-        
-        imgPierre.addEventListener("click", () => {
-            saisieUtilisateur = 1
-        })
-        imgPapier.addEventListener("click", () => {
-            saisieUtilisateur = 2
-        })
-        imgCiseaux.addEventListener("click", () => {
-            saisieUtilisateur = 3
-        })
+        let saisieOrdinateur = Math.ceil(Math.random() * 3)
 
-        saisieOrdinateur = Math.ceil(Math.random() * 3)
-
-        if(saisieUtilisateur === 1 && saisieOrdinateur === 2) {
-            scoreOrdinateur++
+        if ((saisieUtilisateur === 1 && saisieOrdinateur === 3) ||
+            (saisieUtilisateur === 2 && saisieOrdinateur === 1) ||
+            (saisieUtilisateur === 3 && saisieOrdinateur === 2)) {
+            scoreUtilisateur++
         }
-            else if (saisieUtilisateur === 1 && saisieOrdinateur === 3) {
-                scoreUtilisateur++
-            }
-            else if (saisieUtilisateur === 2 && saisieOrdinateur === 1) {
-                scoreUtilisateur++
-            }
-            else if (saisieUtilisateur === 2 && saisieOrdinateur === 3) {
+        else if ((saisieUtilisateur === 1 && saisieOrdinateur === 2) ||
+                (saisieUtilisateur === 2 && saisieOrdinateur === 3) ||
+                (saisieUtilisateur === 3 && saisieOrdinateur === 1)) {
                 scoreOrdinateur++
-            }
-            else if (saisieUtilisateur === 3 && saisieOrdinateur === 1) {
-                scoreOrdinateur++
-            }
-            else if (saisieUtilisateur === 3 && saisieOrdinateur === 2) {
-                scoreUtilisateur++
-            }
+        }
 
             zoneScoreUtilisateur.innerText = scoreUtilisateur
             zoneScoreOrdinateur.innerText = scoreOrdinateur
     }
 
+    saisieUtilisateur = 0
+    saisieOrdinateur = 0
+
     wrapper.style.display = "none"
     zoneScore.style.display = "none"
     zoneResultat.style.display = "block"
-    let winner = document.createElement("p")
 
     if(scoreUtilisateur === 3) {
-        winner.innerText = "Vous avez gagné !"
+        affichageGagnant.innerText = "Vous avez gagné !"
     }
     else if(scoreOrdinateur === 3) {
-        winner.innerText = "Vous avez perdu !"
+        affichageGagnant.innerText = "Vous avez perdu !"
     }
-    zoneResultat.appendChild(winner)
+    zoneResultat.appendChild(affichageGagnant)
 
     btnCommencer.style.display = "block"
-
 }
 
