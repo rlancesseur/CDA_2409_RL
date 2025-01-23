f49edd1418d68e9dd67ecc506445794aeb922775
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-01-23 15:55:49 +0100

    Update 03_TP_architecture_Requêtes.sql

--- 03 - Databases/TP-Architecture/03_TP_architecture_Requêtes.sql
+++ 03 - Databases/TP-Architecture/03_TP_architecture_Requêtes.sql
@@ -1,3 +1,5 @@
+USE db_architecte;
+
 /* 1. Sélectionner l'identifiant, le nom de tous les clients dont le numéro de téléphone commence par '04' */
 SELECT client_ref, client_nom
 FROM clients
@@ -28,13 +30,10 @@ JOIN employes ON employes.emp_matricule = projets.emp_matricule
 JOIN fonctions ON fonctions.fonction_id = employes.fonction_id;
 
 /* 6. Sélectionner tous les projets (dates, superficies, prix) avec le nombre d'intervenants autres que le client et l'architecte */
-SELECT projet_date_depot, projet_superficie_totale, projet_prix, COUNT(participer.emp_matricule)
+SELECT participer.projet_ref, projet_date_depot, projet_superficie_totale, projet_prix, COUNT(participer.emp_matricule)
 FROM projets
-JOIN employes ON employes.emp_matricule = projets.emp_matricule
-JOIN participer ON participer.emp_matricule = projets.emp_matricule
-JOIN fonctions ON fonctions.fonction_id = employes.fonction_id
-WHERE fonctions.fonction_nom != "Architecte"
-GROUP BY projets.projet_ref;
+JOIN participer ON participer.projet_ref = projets.projet_ref
+GROUP BY participer.projet_ref;
 
 /* 7. Sélectionner les types de projets avec, pour chacun d'entre eux, le nombre de projets associés et le prix moyen pratiqué */
 SELECT tp.type_projet_id, tp.type_projet_libelle, COUNT(projet_ref), AVG(projet_prix)
@@ -49,10 +48,16 @@ JOIN projets ON projets.type_travaux_id = tp.type_travaux_id
 GROUP BY tp.type_travaux_id;
 
 /* 9. Sélectionner l'ensembles des projets (dates, prix) avec les informations du client (nom, telephone, adresse), le type de travaux et le type de projet. */
-SELECT projet_date_depot, projet_prix, client_nom, client_telephone, adresse_ville
+SELECT projet_date_depot, projet_prix, client_nom, client_telephone, adresse_ville, type_travaux_libelle, type_projet_libelle
 FROM projets
 JOIN clients ON clients.client_ref = projets.client_ref
-JOIN adresses ON adresses.adresse_id = projets.adresse_id;
+JOIN adresses ON adresses.adresse_id = projets.adresse_id
+JOIN type_travaux ON type_travaux.type_travaux_id = projets.type_travaux_id
+JOIN type_projets ON type_projets.type_projet_id = projets.type_projet_id;
 
 /* 10. Sélectionner les projets dont l'adresse est identique au client associé */
-SELECT
+SELECT projet_ref, projets.adresse_id, clients.adresse_id
+FROM projets
+JOIN clients ON clients.client_ref = projets.client_ref
+JOIN adresses ON adresses.adresse_id = projets.adresse_id
+WHERE projets.adresse_id = clients.adresse_id;
