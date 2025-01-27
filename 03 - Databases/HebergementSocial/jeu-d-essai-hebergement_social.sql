729f015b256d429cdd4cb72d8bfea2d96773bce9
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-01-27 14:14:34 +0100

    Update Hebergement social

--- 03 - Databases/HebergementSocial/jeu-d-essai-hebergement_social.sql
+++ 03 - Databases/HebergementSocial/jeu-d-essai-hebergement_social.sql
@@ -46,10 +46,16 @@ SET individu_id_medecin = 7
 WHERE individu_id = 15;
 
 ALTER TABLE individu
-ADD CONSTRAINT fk_individu_medecin FOREIGN KEY (individu_id_medecin) REFERENCES individu(individu_id_medecin); 
+ADD CONSTRAINT fk_individu_medecin FOREIGN KEY(individu_id_medecin) REFERENCES individu(individu_id); 
 
 INSERT INTO activite
-(activite_intitule, activite_date, activite_heure_debut, activite_heure_fin, activite_nbr_participant_min, activite_nbr_participant_max, activite_etat, individu_id)
+(activite_intitule, activite_date, activite_heure_debut, activite_heure_fin, activite_nbr_participant_min, activite_nbr_participant_max, activite_etat)
 VALUES
-("Natation", "2025-01-29", "18:00", "20:00", 2, 20, "ouvert", 9),
-("Musculation", "2025-03-29", "19:00", "21:00", 1, 10, "ouvert", 10);
+("Natation", "2025-01-29", "18:00", "20:00", 2, 20, "ouvert"),
+("Musculation", "2025-03-29", "19:00", "21:00", 1, 10, "ouvert");
+
+INSERT INTO animer
+(individu_id, activite_id)
+VALUES
+(9, 1),
+(11, 2);
