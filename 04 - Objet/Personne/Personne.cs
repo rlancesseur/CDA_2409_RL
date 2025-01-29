193a8c2923c9fa8b4e33c8e28fb18dd20a9318f5
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-01-29 15:55:45 +0100

    Add Personne

---
+++ 04 - Objet/Personne/Personne.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personne
+{
+    public class Personne
+    {
+        public string nom;
+        public string prenom;
+        public int age;
+        public float salaire;
+        public float autreRevenus;
+        public static int nbPersonnes = 0;
+
+        public Personne()
+        {
+            nom = "Doe";
+            prenom = "John";
+            age = 18;
+            salaire = 1000;
+            autreRevenus = 200;
+        }
+
+        public Personne(string _nom, string _prenom, int _age, float _salaire, float _autreRevenus)
+        {
+            this.nom = _nom;
+            this.prenom = _prenom;
+            this.age = _age;
+            this.salaire = _salaire;
+            this.autreRevenus = _autreRevenus;
+
+            nbPersonnes++;
+        }
+
+        public void afficher()
+        {
+            Console.WriteLine("Nom : " + this.nom);
+            Console.WriteLine("Prénom : " + this.prenom);
+            Console.WriteLine("Age : " + this.age);
+            Console.WriteLine("Salaire : " + this.salaire);
+            Console.WriteLine("Autre revenus : " + this.autreRevenus);
+        }
+
+        public void modifierRevenus(float nouveauSalaire, float nouveauxAutresRevenus)
+        {
+            this.salaire = nouveauSalaire;
+            this.autreRevenus = nouveauxAutresRevenus;
+        }
+
+        public float calculerCharges()
+        {
+            float result = ((this.salaire * 20) / 100) + ((this.autreRevenus * 15) / 100);
+            if (this.age > 50)
+            {
+                result = ((this.salaire * 10) / 100) + ((this.autreRevenus * 7.5f) / 100);
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ 04 - Objet/Personne/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Personne
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne personne1 = new("Lancesseur", "Rémi", 33, 2000, 500);
+            Personne personne2 = new("Doe", "Jane", 25, 1500, 300);
+            Personne personne3 = new("tata", "toto", 60, 2000, 500);
+            Personne personne4 = new("srhrsh", "Jsrharne", 30, 5000, 3000);
+
+
+            personne1.modifierRevenus(3000, 400);
+            personne1.afficher();
+            float charges = personne1.calculerCharges();
+            Console.WriteLine("Charges : " + charges);
+
+            Console.WriteLine("Nombre de personnes : " + Personne.nbPersonnes);
+        }
+    }
+}
