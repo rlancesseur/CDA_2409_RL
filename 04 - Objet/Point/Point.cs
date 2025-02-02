d9f35412dae47f9c86b7865361f4f80dd072b3a6
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-02-02 13:49:21 +0100

    Add exercice objet Point

---
+++ 04 - Objet/Point/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    public class Point
+    {
+        float x;
+        float y;
+
+        public Point()
+        {
+            x = 5;
+            y = 5;
+        }
+        public Point(float _x, float _y)
+        {
+            this.x = _x;
+            this.y = _y;
+        }
+
+        public void afficher()
+        {
+            Console.WriteLine("x = " + this.x + ", y = " + this.y);
+        }
+
+        public void deplacer(float nouveauX, float nouveauY)
+        {
+            this.x = nouveauX;
+            this.y = nouveauY;
+        }
+
+    }
+}
