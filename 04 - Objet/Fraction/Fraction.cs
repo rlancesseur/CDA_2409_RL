903569e7e64969726f46e6b2d71f6b1afd51557d
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-02-03 15:41:40 +0100

    Add exercices objet

---
+++ 04 - Objet/Fraction/Fraction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    internal class Fraction
+    {
+        int numerateur;
+        int denominateur;
+
+        public Fraction()
+        {
+            numerateur = 0;
+            denominateur = 1;
+        }
+
+        public Fraction(int _numerateur, int _denominateur)
+        {
+            this.numerateur = _numerateur;
+            this.denominateur = _denominateur;
+        }
+
+        public Fraction(int _numerateur)
+        {
+            this.numerateur = _numerateur;
+            this.denominateur = 1;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Numérateur : " + this.numerateur + ", Dénominateur : " + this.denominateur);
+        }
+
+        public string AffichageTextuelle()
+        {
+            string result;
+            if(this.denominateur == 1)
+            {
+                result = this.numerateur + "";
+            }
+
+            else if(this.numerateur == 0)
+            {
+                result = 0 + "";
+            }
+            else
+            {
+                result = this.numerateur + "/" + this.denominateur;
+            }
+            return result;
+        }
+
+        public void Oppose()
+        {
+            this.numerateur = -(this.numerateur);
+        }
+
+        public void Inverse()
+        {
+            int pot = this.numerateur;
+            this.numerateur = this.denominateur;
+            this.denominateur = pot;
+        }
+    }
+}
---
+++ 04 - Objet/Fraction/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Fraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new(12, 7);
+            f1.Afficher();
+
+            Fraction f2 = new();
+            f2.Afficher();
+
+            Fraction f3 = new Fraction(9);
+            f3.Afficher();
+
+            string affichage = f1.AffichageTextuelle();
+            Console.WriteLine(affichage);
+            affichage = f2.AffichageTextuelle();
+            Console.WriteLine(affichage);
+            affichage = f3.AffichageTextuelle();
+            Console.WriteLine(affichage);
+        }
+    }
+}
