4aa6e908124c184c5a4de8a0046871e777f0a316
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-02-03 15:40:19 +0100

    Update exercices objet

--- 04 - Objet/Point/Program.cs
+++ 04 - Objet/Point/Program.cs
@@ -10,6 +10,21 @@
             point1.deplacer(20, 25);
             point1.afficher();
 
+            Point point2 = point1.symetrieX();
+            point2.afficher();
+
+            Point point3 = point1.symetrieY();
+            point3.afficher();
+
+            Point point4 = point3.symetriePoint();
+            point4.afficher();
+
+            point4.permuter();
+            point4.afficher();
+
+            point4.deplacer(-30, 30);
+            point4.afficher();
+
         }
     }
 }
