1b8bbeadf6e2488d27eaf4c704dd0b575c460337
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-02-04 12:51:27 +0100

    Update exercice objet

--- 04 - Objet/Fraction/Fraction.cs
+++ 04 - Objet/Fraction/Fraction.cs
@@ -64,5 +64,120 @@ namespace Fraction
             this.numerateur = this.denominateur;
             this.denominateur = pot;
         }
+
+        public bool SuperieurA(Fraction _fraction)
+        {
+            bool result = false;
+            if ((this.numerateur / this.denominateur) > (_fraction.numerateur / _fraction.denominateur))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public bool EgalA(Fraction _fraction)
+        {
+            bool result = false;
+            if ((this.numerateur / this.denominateur) == (_fraction.numerateur / _fraction.denominateur))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private int GetPgcd()
+        {
+            int a = this.numerateur;
+            int b = this.denominateur;
+            int pgcd = 1;
+            if (a != 0 && b != 0)
+            {
+                if (a < 0) a = -a;
+                if (b < 0) b = -b;
+                while (a != b)
+                {
+                    if (a < b)
+                    {
+                        b = b - a;
+                    }
+                    else
+                    {
+                        a = a - b;
+                    }
+                }
+                pgcd = a;
+            }
+            return pgcd;
+        }
+
+        private void Reduire()
+        {
+            
+        }
+
+        public Fraction Plus(Fraction _fraction)
+        {
+            int nouveauNumerateur = 0;
+            int nouveauDenominateur = 0;
+
+            if (this.denominateur < _fraction.denominateur)
+            {
+                int i = 0;
+                while (this.denominateur != _fraction.denominateur)
+                {
+                    nouveauDenominateur = this.denominateur * i;
+                    i++;
+                }
+                nouveauNumerateur = this.denominateur * i;
+            }
+
+            else if (this.denominateur > _fraction.denominateur)
+            {
+                int i = 0;
+                while (this.denominateur != _fraction.denominateur)
+                {
+                    nouveauDenominateur = _fraction.denominateur * i;
+                    i++;
+                }
+                nouveauNumerateur = _fraction.denominateur * i;
+            }
+
+            Fraction nouvelleFraction = new(nouveauNumerateur, nouveauDenominateur);
+            return nouvelleFraction;
+        }
+
+        public Fraction Moins(Fraction _fraction)
+        {
+            int nouveauNumerateur = 0;
+            int nouveauDenominateur = 0;
+
+            nouveauNumerateur = ((this.numerateur * _fraction.denominateur) - (this.denominateur * _fraction.numerateur));
+            nouveauDenominateur = this.denominateur * _fraction.denominateur;
+
+            Fraction nouvelleFraction = new(nouveauNumerateur, nouveauDenominateur);
+            return nouvelleFraction;
+        }
+
+        public Fraction Multiplie(Fraction _fraction)
+        {
+            int nouveauNumerateur = this.numerateur * _fraction.numerateur;
+            int nouveauDenominateur = this.denominateur * _fraction.denominateur;
+
+            Fraction nouvelleFraction = new(nouveauNumerateur, nouveauDenominateur);
+            return nouvelleFraction;
+        }
+
+        public Fraction Divise(Fraction _fraction)
+        {
+            int nouveauNumerateur = 0;
+            int nouveauDenominateur = 0;
+
+            nouveauNumerateur = (this.numerateur * _fraction.denominateur);
+            nouveauDenominateur = this.denominateur * _fraction.denominateur;
+
+            Fraction nouvelleFraction = new(nouveauNumerateur, nouveauDenominateur);
+            return nouvelleFraction;
+        }
+
     }
 }
--- 04 - Objet/Fraction/Program.cs
+++ 04 - Objet/Fraction/Program.cs
@@ -13,12 +13,24 @@
             Fraction f3 = new Fraction(9);
             f3.Afficher();
 
-            string affichage = f1.AffichageTextuelle();
-            Console.WriteLine(affichage);
-            affichage = f2.AffichageTextuelle();
-            Console.WriteLine(affichage);
-            affichage = f3.AffichageTextuelle();
-            Console.WriteLine(affichage);
+            Console.WriteLine(f1.AffichageTextuelle());
+            Console.WriteLine(f2.AffichageTextuelle());
+            Console.WriteLine(f3.AffichageTextuelle());
+
+            f1.Oppose();
+            f1.Afficher();
+
+            f1.Inverse();
+            f1.Afficher();
+
+            bool estSuperieur = f2.SuperieurA(f3);
+            Console.WriteLine(estSuperieur ? "f2 est supérieur à f3." : "f3 est supérieur à f2.");
+
+            Fraction f4 = new(18, 2);
+            bool estEgal = f3.EgalA(f4);
+            Console.WriteLine(estEgal ? "f3 est égal à f4." : "f3 n'est pas égal à f4.");
+
+            
         }
     }
 }
