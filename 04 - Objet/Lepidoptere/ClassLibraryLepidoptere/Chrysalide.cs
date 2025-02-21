7e4711049ea8970c7e2f7baefe58754a0c44ca77
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-02-21 12:01:48 +0100

    Add Lepidoptere

---
+++ 04 - Objet/Lepidoptere/ClassLibraryLepidoptere/Chrysalide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryLepidoptere
+{
+    public class Chrysalide : StadeEvolution
+    {
+        public Chrysalide()
+        {
+
+        }
+
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je ne peux pas.");
+        }
+
+        public override StadeEvolution SeMetamorphoser()
+        {
+            return new Papillon();
+        }
+
+    }
+}
---
+++ 04 - Objet/Lepidoptere/ConsoleAppLepidoptere/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibraryLepidoptere;
+
+namespace ConsoleAppLepidoptere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lepidoptere monLepi = new("Bembecia ichneumoniformis");
+            monLepi.SeDeplacer();
+            monLepi.SeMetamorphoser();
+            monLepi.SeDeplacer();
+            monLepi.SeMetamorphoser();
+            monLepi.SeDeplacer();
+            monLepi.SeMetamorphoser();
+            monLepi.SeDeplacer();
+        }
+    }
+}
