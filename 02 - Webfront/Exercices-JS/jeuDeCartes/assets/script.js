5f8491a24cd50a3accee17d433260729a3ca52d8
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-03-05 17:29:46 +0100

    Update script.js

--- 02 - Webfront/Exercices-JS/jeuDeCartes/assets/script.js
+++ 02 - Webfront/Exercices-JS/jeuDeCartes/assets/script.js
@@ -2,55 +2,21 @@ const afficherTableau = () => {
     fetch("https://arfp.github.io/tp/web/javascript/03-cardgame/cardgame.json")
     .then(response => response.json())
     .then(cartes => {
-        for(let carte of cartes) {
-            const tbody = document.querySelector("tbody")
-            const tr = document.createElement("tr")
-            const tdId = document.createElement("td")
-            const tdName = document.createElement("td")
-            const tdLevel = document.createElement("td")
-            const tdDescription = document.createElement("td")
-            const tdPower = document.createElement("td")
-            const tdAttack = document.createElement("td")
-            const tdArmor = document.createElement("td")
-            const tdDamage = document.createElement("td")
-            const tdMitigation = document.createElement("td")
-            const tdPlayed = document.createElement("td")
-            const tdVictory = document.createElement("td")
-            const tdDefeat = document.createElement("td")
-            const tdDraw = document.createElement("td")
+        const tbody = document.querySelector("tbody")
+        const champs = ["id", "name", "level", "description", "power", "attack", "armor", "damage", "mitigation", "played", "victory", "defeat", "draw"]
 
-            tdId.innerText = carte.id
-            tdName.innerText = carte.name
-            tdLevel.innerText = carte.level
-            tdDescription.innerText = carte.description || ""
-            tdPower.innerText = carte.power
-            tdAttack.innerText = carte.attack
-            tdArmor.innerText = carte.armor
-            tdDamage.innerText = carte.damage
-            tdMitigation.innerText = carte.mitigation
-            tdPlayed.innerText = carte.played
-            tdVictory.innerText = carte.victory
-            tdDefeat.innerText = carte.defeat
-            tdDraw.innerText = carte.draw
-
-            tr.appendChild(tdId)
-            tr.appendChild(tdName)
-            tr.appendChild(tdLevel)
-            tr.appendChild(tdDescription)
-            tr.appendChild(tdPower)
-            tr.appendChild(tdAttack)
-            tr.appendChild(tdArmor)
-            tr.appendChild(tdDamage)
-            tr.appendChild(tdMitigation)
-            tr.appendChild(tdPlayed)
-            tr.appendChild(tdVictory)
-            tr.appendChild(tdDefeat)
-            tr.appendChild(tdDraw)
-            tbody.appendChild(tr)
+        for (const carte of cartes) {
+            const tr = document.createElement("tr");
+            for (const champ of champs) {
+                const td = document.createElement("td");
+                td.innerText = carte[champ] || "";
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
         }
 
-        algo1(cartes)
-        algo2(cartes)
+    algo1(cartes)
+    algo2(cartes)
     })
 }
 
