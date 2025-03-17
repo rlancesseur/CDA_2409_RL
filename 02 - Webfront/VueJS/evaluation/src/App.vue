472a3fc7cd7187cedfc6a703fba9dcd76faa71b8
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-03-17 20:03:28 +0100

    Update evaluation

--- 02 - Webfront/VueJS/evaluation/src/App.vue
+++ 02 - Webfront/VueJS/evaluation/src/App.vue
@@ -1,7 +1,14 @@
 <template>
     <h1>Résultat de l'évaluation</h1>
 
-    <Formulaire :ajouterEtudiant="ajouterEtudiant" />
+    <form action="" @submit.prevent="ajouterEtudiant">
+        <p>Ajouter une note</p>
+
+        <LabelInput label="Nom" v-model="nomEtudiant" inputType="text" />
+        <LabelInput label="Note" v-model="noteEtudiant" inputType="number" />
+        
+        <input id="bntSubmit" type="submit">
+    </form>
 
     <table>
         <thead>
@@ -30,57 +37,62 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import Formulaire from './components/Formulaire.vue'
+import LabelInput from './components/LabelInput.vue'
+import { fetchUsers } from './utils/fetchUsers'
 
 const evalData = ref([])
+const nomEtudiant = ref('')
+const noteEtudiant = ref(null)
 const nbEtudiant = ref(0)
 const moyClass = ref(0)
 const nbDessusMoy = ref(0)
 const noteEliminatoire = 12
 
-onMounted(() => {
-    fetch('./public/eval.json')
-        .then((r) => {
-            if (r.ok) {
-                return r.json()
-            }
-            throw new Error('Impossible de récupérer le json')
-        })
-        .then((data) => {
-            evalData.value = data
-            display(evalData.value)
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-})
-
-const display = (ev) => {
-    nbEtudiant.value = ev.length
-    let totalNote = 0
-    nbDessusMoy.value = 0
-
-    ev.sort((a, b) => (a.grade < b.grade ? 1 : -1)).forEach((element) => {
-        totalNote += parseFloat(element.grade)
-        element.isPassed = element.grade >= noteEliminatoire
-    })
-
-    moyClass.value = (totalNote / nbEtudiant.value).toFixed(2)
-
-    ev.forEach((element) => {
-        if (element.grade > moyClass.value) {
-            nbDessusMoy.value++
-        }
-    })
+const getUsers = async () => {
+    try {
+        const data = await fetchUsers()
+        evalData.value = data.sort((a, b) => (a.grade < b.grade ? 1 : -1))
+        calculStat()
+    } catch (e) {
+        console.error('Erreur lors du chargement des données :', e)
+    }
 }
 
-const ajouterEtudiant = (nom, note) => {
-    if (nom.length >= 2 && note >= 0 && note <= 20) {
+onMounted(() => {
+    getUsers()
+})
+
+//A REFAIRE
+// const calculStat = () => {
+//     nbEtudiant.value = evalData.value.length
+//     let totalNote = 0
+//     nbDessusMoy.value = 0
+
+//     evalData.value.forEach((element) => {
+//         totalNote += parseFloat(element.grade)
+//         element.isPassed = element.grade >= noteEliminatoire
+//     })
+
+//     moyClass.value = (totalNote / nbEtudiant.value).toFixed(2)
+
+//     evalData.value.forEach((element) => {
+//         if (element.grade > moyClass.value) {
+//             nbDessusMoy.value++
+//         }
+//     })
+// }
+
+const ajouterEtudiant = () => {
+    if (nomEtudiant.value.length >= 2 && noteEtudiant.value >= 0 && noteEtudiant.value <= 20) {
         evalData.value.push({
-            fullname: nom,
-            grade: note,
+            fullname: nomEtudiant.value,
+            grade: noteEtudiant.value,
         })
-        display(evalData.value)
-    }
+        evalData.value.sort((a, b) => (a.grade < b.grade ? 1 : -1))
+
+        nomEtudiant.value = ''
+        noteEtudiant.value = null
+        calculStat()
+    }    
 }
 </script>
--- 02 - Webfront/VueJS/evaluation/src/components/LabelInput.vue
+++ 02 - Webfront/VueJS/evaluation/src/components/LabelInput.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="labelInput">
         <label :for="label">{{ label }} :</label>
-        <input :type="inputType" :value="model" :id="label" />
+        <input :type="inputType" :value="modelValue" :id="label" @input="onInputChange"/>
     </div>
 </template>
 
 <script setup>
 const props = defineProps({
     label: String,
-    model: String,
+    modelValue: [String, Number],
     inputType: String,
 })
+
+const emit = defineEmits(['update:modelValue'])
+
+const onInputChange = (event) => {
+    emit('update:modelValue', event.target.value)
+}
+
 </script>
