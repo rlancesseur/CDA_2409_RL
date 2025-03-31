6f511376c6e7c254074b7661b6d2ce4d373d8b2e
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-03-31 15:57:10 +0200

    Add distributeurDeBoissons

---
+++ 02 - Webfront/VueJS/distributeurDeBoissons/data/boissons.js
@@ -0,0 +1,34 @@
+export const boissons =
+
+[
+    {
+        "nom": "Soda Cola",
+        "tarif": 1.25,
+        "stock": 20
+    },
+    {
+        "nom": "Eau plate",
+        "tarif": 1.05,
+        "stock": 25
+    },
+    {
+        "nom": "Eau gazeuse",
+        "tarif": 1.15,
+        "stock": 30
+    },
+    {
+        "nom": "Jus d'orange",
+        "tarif": 1.25,
+        "stock": 35
+    },
+    {
+        "nom": "Soda Citron",
+        "tarif": 1.25,
+        "stock": 0
+    },
+    {
+        "nom": "Café frappé",
+        "tarif": 1.35,
+        "stock": 50
+    }
+]
