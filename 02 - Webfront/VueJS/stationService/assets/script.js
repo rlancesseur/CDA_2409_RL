72c53c531df18f58c4c05b82e8980ae9a0c71fed
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-04-01 15:54:58 +0200

    Add stationService

---
+++ 02 - Webfront/VueJS/stationService/assets/script.js
@@ -0,0 +1,9 @@
+const myApp = {
+    data() {
+        return {
+            
+        }
+    }
+}
+
+Vue.createApp(myApp).mount("#app")
---
+++ 02 - Webfront/VueJS/stationService/data/carburants.js
@@ -0,0 +1,8 @@
+export const carburants = 
+[
+    {
+        'nom': 'Gasoil',
+        'contenue': 10000,
+        'prix': 1.879
+    }
+]
