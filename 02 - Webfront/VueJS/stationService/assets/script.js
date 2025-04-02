e3e707cf1c8412d4581c8d6a926f42e48292cf09
Rémi Lancesseur <lancesseur.remi@yahoo.fr>
2025-04-02 15:50:08 +0200

    Update stationService

--- 02 - Webfront/VueJS/stationService/assets/script.js
+++ 02 - Webfront/VueJS/stationService/assets/script.js
@@ -3,7 +3,65 @@ import { carburants } from "../data/carburants.js"
 const myApp = {
     data() {
         return {
-            listeCarbu: carburants
+            listeCarbu: carburants,
+            selectedCarburant: "",
+            limite: 0,
+            isLitres: false,
+            isEuros: false,
+            listeFactures: []
+        }
+    },
+    computed: {
+        carburantSelectionne() {
+            return this.listeCarbu.find(carburant => carburant.nom === this.selectedCarburant) || {}
+        },
+        prixDuLitre() {
+            return this.carburantSelectionne.prix || 0
+        },
+        limiteEnEuros() {
+            if (this.isEuros) {
+                return this.limite / this.prixDuLitre
+            }
+            return this.limite
+        },
+        quantiteAVider() {
+            if (this.isEuros) {
+                return this.limite / this.prixDuLitre
+            }
+            return this.limite
+        },
+        totalPaye() {
+            return this.listeFactures.reduce((a,b) => a + parseFloat(b.quantite), 0)
+        },
+    },
+    methods: {
+        lancerDistrib() {
+            let carburantSelectionne = this.listeCarbu.find((carburant) => carburant.nom === this.selectedCarburant)
+            if(!this.isLitres && !this.isEuros) {
+                return
+            }
+
+            if (this.isLitres && carburantSelectionne.contenu >= this.limite) {
+                carburantSelectionne.contenu -= this.limite
+            }
+            else if (this.isEuros && carburantSelectionne.contenu >= this.limiteEnLitres) {
+                carburantSelectionne.contenu -= this.limiteEnEuros
+            }
+
+            this.listeFactures.push({
+                date: new Date(),
+                carburant: this.selectedCarburant,
+                prix: this.prixDuLitre,
+                quantite: this.limite
+            })
+        },
+        formatDate(date) {
+            const jour = String(date.getDate()).padStart(2, '0')
+            const mois = String(date.getMonth()+1).padStart(2, '0')
+            const heure = String(date.getHours()).padStart(2, '0')
+            const minutes = String(date.getMinutes()).padStart(2, '0')
+            const secondes = String(date.getSeconds()).padStart(2, '0')
+            return jour + '/' + mois + '/' + date.getFullYear() + ' ' + heure + ':' + minutes + ':' + secondes
         }
     }
 }
