15e1db9969cd0cb77734dc7eede80cfe177e24e4
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-05-05 15:53:22 +0200

    Add VoitureBis

---
+++ 04 - Objet/ClassLibraryVoitureBis/Roue.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassLibraryVoitureBis
+{
+    public class Roue
+    {
+        string marqueRoue;
+        bool tourne;
+        float usureEnMm;
+
+        public Roue(string _marqueRoue, bool _tourne, float _usureEnMm)
+        {
+            marqueRoue = _marqueRoue;
+            tourne = _tourne;
+            usureEnMm = _usureEnMm;
+        }
+
+        public Roue() : this("Michelin", false, 0)
+        {
+        }
+
+        public Roue(Roue roueACopier)
+        {
+            marqueRoue = roueACopier.marqueRoue;
+            tourne = roueACopier.tourne;
+            usureEnMm = roueACopier.usureEnMm;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Marque : " + marqueRoue + "; tourne : " + tourne + "; usure : " + usureEnMm;
+        }
+    }
+}
