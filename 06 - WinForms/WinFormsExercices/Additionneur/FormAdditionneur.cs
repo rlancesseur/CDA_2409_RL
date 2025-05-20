c9ace11ad09746178457363257103aabe2ef6d1f
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-05-20 15:54:37 +0200

    Add Ex WinForms

---
+++ 06 - WinForms/WinFormsExercices/Additionneur/FormAdditionneur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Additionneur
+{
+    public partial class FormAdditionneur : Form
+    {
+        int result = 0;
+
+        public FormAdditionneur()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int value = int.Parse(btn.Text);
+            additionneurTextBox.Text += value + "+";
+            result += value;
+        }
+
+        private void buttonCalculer_Click(object sender, EventArgs e)
+        {
+            additionneurTextBox.Text += " = " + result + "+";
+        }
+
+        private void buttonVider_Click(object sender, EventArgs e)
+        {
+            additionneurTextBox.Clear();
+            result = 0;
+        }
+    }
+}
---
+++ 06 - WinForms/WinFormsExercices/ValidationSaisie/FormValidationSaisie.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ValidationSaisie
+{
+    partial class FormValidationSaisie
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNom = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.textBoxMontant = new System.Windows.Forms.TextBox();
+            this.labelMontant = new System.Windows.Forms.Label();
+            this.textBoxCodePostal = new System.Windows.Forms.TextBox();
+            this.labelCodePostal = new System.Windows.Forms.Label();
+            this.buttonValider = new System.Windows.Forms.Button();
+            this.buttonEffacer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNom.Location = new System.Drawing.Point(34, 73);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(37, 17);
+            this.labelNom.TabIndex = 0;
+            this.labelNom.Text = "Nom";
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNom.Location = new System.Drawing.Point(140, 72);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNom.TabIndex = 1;
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDate.Location = new System.Drawing.Point(140, 114);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(100, 23);
+            this.textBoxDate.TabIndex = 3;
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(34, 115);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(38, 17);
+            this.labelDate.TabIndex = 2;
+            this.labelDate.Text = "Date";
+            // 
+            // textBoxMontant
+            // 
+            this.textBoxMontant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMontant.Location = new System.Drawing.Point(140, 159);
+            this.textBoxMontant.Name = "textBoxMontant";
+            this.textBoxMontant.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMontant.TabIndex = 5;
+            // 
+            // labelMontant
+            // 
+            this.labelMontant.AutoSize = true;
+            this.labelMontant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMontant.Location = new System.Drawing.Point(34, 160);
+            this.labelMontant.Name = "labelMontant";
+            this.labelMontant.Size = new System.Drawing.Size(59, 17);
+            this.labelMontant.TabIndex = 4;
+            this.labelMontant.Text = "Montant";
+            // 
+            // textBoxCodePostal
+            // 
+            this.textBoxCodePostal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCodePostal.Location = new System.Drawing.Point(140, 203);
+            this.textBoxCodePostal.Name = "textBoxCodePostal";
+            this.textBoxCodePostal.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCodePostal.TabIndex = 7;
+            // 
+            // labelCodePostal
+            // 
+            this.labelCodePostal.AutoSize = true;
+            this.labelCodePostal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodePostal.Location = new System.Drawing.Point(34, 204);
+            this.labelCodePostal.Name = "labelCodePostal";
+            this.labelCodePostal.Size = new System.Drawing.Size(84, 17);
+            this.labelCodePostal.TabIndex = 6;
+            this.labelCodePostal.Text = "Code Postal";
+            // 
+            // buttonValider
+            // 
+            this.buttonValider.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonValider.Location = new System.Drawing.Point(313, 279);
+            this.buttonValider.Name = "buttonValider";
+            this.buttonValider.Size = new System.Drawing.Size(83, 32);
+            this.buttonValider.TabIndex = 8;
+            this.buttonValider.Text = "Valider";
+            this.buttonValider.UseVisualStyleBackColor = true;
+            this.buttonValider.Click += new System.EventHandler(this.buttonValider_Click);
+            // 
+            // buttonEffacer
+            // 
+            this.buttonEffacer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEffacer.Location = new System.Drawing.Point(313, 332);
+            this.buttonEffacer.Name = "buttonEffacer";
+            this.buttonEffacer.Size = new System.Drawing.Size(83, 32);
+            this.buttonEffacer.TabIndex = 9;
+            this.buttonEffacer.Text = "Effacer";
+            this.buttonEffacer.UseVisualStyleBackColor = true;
+            // 
+            // FormValidationSaisie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 385);
+            this.Controls.Add(this.buttonEffacer);
+            this.Controls.Add(this.buttonValider);
+            this.Controls.Add(this.textBoxCodePostal);
+            this.Controls.Add(this.labelCodePostal);
+            this.Controls.Add(this.textBoxMontant);
+            this.Controls.Add(this.labelMontant);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.labelNom);
+            this.Name = "FormValidationSaisie";
+            this.Text = "Les contrôles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.TextBox textBoxMontant;
+        private System.Windows.Forms.Label labelMontant;
+        private System.Windows.Forms.TextBox textBoxCodePostal;
+        private System.Windows.Forms.Label labelCodePostal;
+        private System.Windows.Forms.Button buttonValider;
+        private System.Windows.Forms.Button buttonEffacer;
+    }
+}
+
