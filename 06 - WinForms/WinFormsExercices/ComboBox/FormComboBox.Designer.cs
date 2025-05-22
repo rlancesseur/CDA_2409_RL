30517dde4952cc8f61ee65bd9c6b56dd7d66373b
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-05-22 12:23:40 +0200

    Add ComboBox

---
+++ 06 - WinForms/WinFormsExercices/ComboBox/FormComboBox.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ComboBox
+{
+    partial class FormComboBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSource = new System.Windows.Forms.Label();
+            this.comboBoxSource = new System.Windows.Forms.ComboBox();
+            this.labelCible = new System.Windows.Forms.Label();
+            this.listBoxCible = new System.Windows.Forms.ListBox();
+            this.buttonOneToCible = new System.Windows.Forms.Button();
+            this.buttonAllToCible = new System.Windows.Forms.Button();
+            this.buttonOneToSource = new System.Windows.Forms.Button();
+            this.buttonAllToSource = new System.Windows.Forms.Button();
+            this.buttonToUp = new System.Windows.Forms.Button();
+            this.buttonToDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelSource
+            // 
+            this.labelSource.AutoSize = true;
+            this.labelSource.Location = new System.Drawing.Point(64, 11);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(41, 13);
+            this.labelSource.TabIndex = 0;
+            this.labelSource.Text = "Source";
+            // 
+            // comboBoxSource
+            // 
+            this.comboBoxSource.FormattingEnabled = true;
+            this.comboBoxSource.Location = new System.Drawing.Point(12, 35);
+            this.comboBoxSource.Name = "comboBoxSource";
+            this.comboBoxSource.Size = new System.Drawing.Size(134, 21);
+            this.comboBoxSource.TabIndex = 1;
+            // 
+            // labelCible
+            // 
+            this.labelCible.AutoSize = true;
+            this.labelCible.Location = new System.Drawing.Point(277, 11);
+            this.labelCible.Name = "labelCible";
+            this.labelCible.Size = new System.Drawing.Size(30, 13);
+            this.labelCible.TabIndex = 2;
+            this.labelCible.Text = "Cible";
+            // 
+            // listBoxCible
+            // 
+            this.listBoxCible.FormattingEnabled = true;
+            this.listBoxCible.Location = new System.Drawing.Point(234, 35);
+            this.listBoxCible.Name = "listBoxCible";
+            this.listBoxCible.Size = new System.Drawing.Size(120, 147);
+            this.listBoxCible.TabIndex = 3;
+            // 
+            // buttonOneToCible
+            // 
+            this.buttonOneToCible.Location = new System.Drawing.Point(167, 33);
+            this.buttonOneToCible.Name = "buttonOneToCible";
+            this.buttonOneToCible.Size = new System.Drawing.Size(43, 23);
+            this.buttonOneToCible.TabIndex = 4;
+            this.buttonOneToCible.Text = ">";
+            this.buttonOneToCible.UseVisualStyleBackColor = true;
+            this.buttonOneToCible.Click += new System.EventHandler(this.buttonOneToCible_Click);
+            // 
+            // buttonAllToCible
+            // 
+            this.buttonAllToCible.Location = new System.Drawing.Point(167, 62);
+            this.buttonAllToCible.Name = "buttonAllToCible";
+            this.buttonAllToCible.Size = new System.Drawing.Size(43, 23);
+            this.buttonAllToCible.TabIndex = 5;
+            this.buttonAllToCible.Text = ">>";
+            this.buttonAllToCible.UseVisualStyleBackColor = true;
+            this.buttonAllToCible.Click += new System.EventHandler(this.buttonAllToCible_Click);
+            // 
+            // buttonOneToSource
+            // 
+            this.buttonOneToSource.Location = new System.Drawing.Point(166, 130);
+            this.buttonOneToSource.Name = "buttonOneToSource";
+            this.buttonOneToSource.Size = new System.Drawing.Size(43, 23);
+            this.buttonOneToSource.TabIndex = 6;
+            this.buttonOneToSource.Text = "<";
+            this.buttonOneToSource.UseVisualStyleBackColor = true;
+            this.buttonOneToSource.Click += new System.EventHandler(this.buttonOneToSource_Click);
+            // 
+            // buttonAllToSource
+            // 
+            this.buttonAllToSource.Location = new System.Drawing.Point(166, 159);
+            this.buttonAllToSource.Name = "buttonAllToSource";
+            this.buttonAllToSource.Size = new System.Drawing.Size(43, 23);
+            this.buttonAllToSource.TabIndex = 7;
+            this.buttonAllToSource.Text = "<<";
+            this.buttonAllToSource.UseVisualStyleBackColor = true;
+            this.buttonAllToSource.Click += new System.EventHandler(this.buttonAllToSource_Click);
+            // 
+            // buttonToUp
+            // 
+            this.buttonToUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToUp.Location = new System.Drawing.Point(261, 197);
+            this.buttonToUp.Name = "buttonToUp";
+            this.buttonToUp.Size = new System.Drawing.Size(28, 28);
+            this.buttonToUp.TabIndex = 8;
+            this.buttonToUp.Text = "↑";
+            this.buttonToUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonToDown
+            // 
+            this.buttonToDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToDown.Location = new System.Drawing.Point(295, 197);
+            this.buttonToDown.Name = "buttonToDown";
+            this.buttonToDown.Size = new System.Drawing.Size(28, 28);
+            this.buttonToDown.TabIndex = 9;
+            this.buttonToDown.Text = "↓";
+            this.buttonToDown.UseVisualStyleBackColor = true;
+            // 
+            // FormComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 241);
+            this.Controls.Add(this.buttonToDown);
+            this.Controls.Add(this.buttonToUp);
+            this.Controls.Add(this.buttonAllToSource);
+            this.Controls.Add(this.buttonOneToSource);
+            this.Controls.Add(this.buttonAllToCible);
+            this.Controls.Add(this.buttonOneToCible);
+            this.Controls.Add(this.listBoxCible);
+            this.Controls.Add(this.labelCible);
+            this.Controls.Add(this.comboBoxSource);
+            this.Controls.Add(this.labelSource);
+            this.Name = "FormComboBox";
+            this.Text = "Les listes";
+            this.Load += new System.EventHandler(this.FormComboBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.ComboBox comboBoxSource;
+        private System.Windows.Forms.Label labelCible;
+        private System.Windows.Forms.ListBox listBoxCible;
+        private System.Windows.Forms.Button buttonOneToCible;
+        private System.Windows.Forms.Button buttonAllToCible;
+        private System.Windows.Forms.Button buttonOneToSource;
+        private System.Windows.Forms.Button buttonAllToSource;
+        private System.Windows.Forms.Button buttonToUp;
+        private System.Windows.Forms.Button buttonToDown;
+    }
+}
+
---
+++ 06 - WinForms/WinFormsExercices/ComboBox/FormComboBox.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class FormComboBox : Form
+    {
+        public FormComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void FormComboBox_Load(object sender, EventArgs e)
+        {
+            comboBoxSource.Items.Add("France");
+            comboBoxSource.Items.Add("Belgique");
+            comboBoxSource.Items.Add("Allemagne");
+            comboBoxSource.Items.Add("Japon");
+            comboBoxSource.Items.Add("Portugal");
+            comboBoxSource.Items.Add("Grece");
+        }
+
+        private void buttonOneToCible_Click(object sender, EventArgs e)
+        {
+            listBoxCible.Items.Add(comboBoxSource.SelectedItem);
+            comboBoxSource.Items.Remove(comboBoxSource.SelectedItem);
+        }
+
+        private void buttonAllToCible_Click(object sender, EventArgs e)
+        {
+            foreach(var item in comboBoxSource.Items)
+            {
+                listBoxCible.Items.Add(item);
+            }
+            
+            comboBoxSource.Items.Clear();
+        }
+
+        private void buttonOneToSource_Click(object sender, EventArgs e)
+        {
+            comboBoxSource.Items.Add(listBoxCible.SelectedItem);
+            listBoxCible.Items.Remove(listBoxCible.SelectedItem);
+        }
+
+        private void buttonAllToSource_Click(object sender, EventArgs e)
+        {
+            foreach(var item in listBoxCible.Items)
+            {
+                comboBoxSource.Items.Add(item);
+            }
+            listBoxCible.Items.Clear();
+        }
+    }
+}
