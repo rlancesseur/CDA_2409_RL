4be72e8aa88ffdd6e12adb562747933710e0c3c2
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2025-05-22 11:03:45 +0200

    Update ListBox

--- 06 - WinForms/WinFormsExercices/ListBox/FormListBox.cs
+++ 06 - WinForms/WinFormsExercices/ListBox/FormListBox.cs
@@ -16,5 +16,66 @@ namespace ListBox
         {
             InitializeComponent();
         }
+
+        private void buttonAjoutListe_Click(object sender, EventArgs e)
+        {
+            if(textBoxNouvelElement.Text != "")
+            {
+                if(!listBoxLstList.Items.Contains(textBoxNouvelElement.Text))
+                {
+                    listBoxLstList.Items.Add(textBoxNouvelElement.Text);
+                    textBoxItemsCount.Text = listBoxLstList.Items.Count.ToString();
+                    textBoxNouvelElement.Clear();
+                    textBoxNouvelElement.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Cette valeur est déjà dans la liste.", "Erreur");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez saisir une valeur", "Erreur");
+                textBoxNouvelElement.Focus();
+            }
+        }
+
+        private void listBoxLstList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxSelectedIndex.Text = listBoxLstList.SelectedIndex.ToString();
+            textBoxText.Text = listBoxLstList.Text.ToString();
+        }
+
+        private void buttonVider_Click(object sender, EventArgs e)
+        {
+            listBoxLstList.Items.Clear();
+            textBoxItemsCount.Text = "";
+            textBoxSelectedIndex.Text = "";
+            textBoxText.Text = "";
+        }
+
+        private void buttonSelectionner_Click(object sender, EventArgs e)
+        {
+            int indexDemande;
+            if (int.TryParse(textBoxIndexElement.Text, out indexDemande))
+            {
+                if(indexDemande >= 0 && indexDemande < listBoxLstList.Items.Count)
+                {
+                    listBoxLstList.SelectedIndex = indexDemande;
+                }
+                else
+                {
+                    MessageBox.Show("L'index demandé n'existe pas", "Erreur");
+                    textBoxIndexElement.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Le format de l'Index est incorrect", "Erreur");
+                textBoxIndexElement.Text = "";
+            }
+
+
+        }
     }
 }
